6933c259aa7997adacb1fd6d0f233d8a82c231e7
eshumanohare <eshumanohare@gmail.com>
2023-09-21 14:59:05 +0530

    fixed router test

--- test/uniswap-v2-plugin.test.ts
+++ test/uniswap-v2-plugin.test.ts
@@ -1,140 +1,143 @@
-import Web3, { core } from 'web3';
-import { UniswapV2Plugin } from '../src/uniswap-v2-plugin';
+import Web3, { core } from "web3";
+import { UniswapV2Plugin } from "../src/uniswap-v2-plugin";
 
-describe('UniswapV2Plugin Tests', () => {
-	it('should register UniswapV2Plugin on UniswapV2Context instance', async () => {
-		const web3Context = new Web3('https://eth.public-rpc.com');
-		web3Context.registerPlugin(new UniswapV2Plugin());
-		expect(web3Context['uniswapV2']).toBeDefined();
-	});
+describe("UniswapV2Plugin Tests", () => {
+  it("should register UniswapV2Plugin on UniswapV2Context instance", async () => {
+    const web3Context = new Web3("https://eth.public-rpc.com");
+    web3Context.registerPlugin(new UniswapV2Plugin());
+    expect(web3Context["uniswapV2"]).toBeDefined();
+  });
 
-	describe('Plugin Tests - UniswapV2 - Factory', () => {
-		const requestManagerSendSpy = jest.fn();
+  describe("Plugin Tests - UniswapV2 - Factory", () => {
+    const requestManagerSendSpy = jest.fn();
 
-		let web3Context: Web3;
-		const uniswapV2FactoryAddress =
-			'0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
+    let web3Context: Web3;
+    const uniswapV2FactoryAddress =
+      "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
 
-		let uniswapV2Factory;
+    let uniswapV2Factory;
 
-		const zeroAddress = '0x0000000000000000000000000000000000000000';
-		const feeToSetterAddress = '0x18e433c7Bf8A2E1d0197CE5d8f9AFAda1A771360';
+    const zeroAddress = "0x0000000000000000000000000000000000000000";
+    const feeToSetterAddress = "0x18e433c7Bf8A2E1d0197CE5d8f9AFAda1A771360";
 
-		beforeAll(() => {
-			web3Context = new Web3('https://eth.public-rpc.com');
-			web3Context.registerPlugin(new UniswapV2Plugin());
-			web3Context['uniswapV2'].requestManager.send = requestManagerSendSpy;
-			uniswapV2Factory = web3Context['uniswapV2'].factory(
-				uniswapV2FactoryAddress
-			);
-		});
+    beforeAll(() => {
+      web3Context = new Web3("https://eth.public-rpc.com");
+      web3Context.registerPlugin(new UniswapV2Plugin());
+      web3Context["uniswapV2"].requestManager.send = requestManagerSendSpy;
+      uniswapV2Factory = web3Context["uniswapV2"].factory(
+        uniswapV2FactoryAddress
+      );
+    });
 
-		it('should call Uniswap V2 Factory feeTo method with expected RPC object', async () => {
-			const signature = Web3.utils.sha3('feeTo()')?.slice(0, 10);
-			const data = signature;
+    it("should call Uniswap V2 Factory feeTo method with expected RPC object", async () => {
+      const signature = Web3.utils.sha3("feeTo()")?.slice(0, 10);
+      const data = signature;
 
-			await uniswapV2Factory.methods.feeTo().call();
+      await uniswapV2Factory.methods.feeTo().call();
 
-			const request = {
-				method: 'eth_call',
-				params: [{ input: data, to: uniswapV2FactoryAddress }, 'latest'],
-			};
-			expect(requestManagerSendSpy).toHaveBeenLastCalledWith(request);
-		});
+      const request = {
+        method: "eth_call",
+        params: [{ input: data, to: uniswapV2FactoryAddress }, "latest"],
+      };
+      expect(requestManagerSendSpy).toHaveBeenLastCalledWith(request);
+    });
 
-		it('should call Uniswap V2 Factory feeToSetter with expected RPC object', async () => {
-			const signature = Web3.utils.sha3('feeToSetter()')?.slice(0, 10);
-			const data = signature;
+    it("should call Uniswap V2 Factory feeToSetter with expected RPC object", async () => {
+      const signature = Web3.utils.sha3("feeToSetter()")?.slice(0, 10);
+      const data = signature;
 
-			await uniswapV2Factory.methods.feeToSetter().call();
+      await uniswapV2Factory.methods.feeToSetter().call();
 
-			const request = {
-				method: 'eth_call',
-				params: [
-					{
-						input: data,
-						to: uniswapV2FactoryAddress,
-					},
-					'latest',
-				],
-			};
+      const request = {
+        method: "eth_call",
+        params: [
+          {
+            input: data,
+            to: uniswapV2FactoryAddress,
+          },
+          "latest",
+        ],
+      };
 
-			expect(requestManagerSendSpy).toHaveBeenLastCalledWith(request);
-		});
+      expect(requestManagerSendSpy).toHaveBeenLastCalledWith(request);
+    });
 
-		it('should call Uniswap V2 Factory getPair() with expected RPC object', async () => {
-			const tokenAAddress = '0x8E870D67F660D95d5be530380D0eC0bd388289E1'; // PAX Coin
-			const tokenBAddress = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'; // USDC
+    it("should call Uniswap V2 Factory getPair() with expected RPC object", async () => {
+      const tokenAAddress = "0x8E870D67F660D95d5be530380D0eC0bd388289E1"; // PAX Coin
+      const tokenBAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"; // USDC
 
-			const signature = Web3.utils
-				.sha3('getPair(address,address)')
-				?.slice(0, 10);
+      const signature = Web3.utils
+        .sha3("getPair(address,address)")
+        ?.slice(0, 10);
 
-			const data =
-				signature +
-				tokenAAddress.slice(2).padStart(64, '0').toLowerCase() +
-				tokenBAddress.slice(2).padStart(64, '0').toLowerCase();
+      const data =
+        signature +
+        tokenAAddress.slice(2).padStart(64, "0").toLowerCase() +
+        tokenBAddress.slice(2).padStart(64, "0").toLowerCase();
 
-			await uniswapV2Factory.methods
-				.getPair(tokenAAddress, tokenBAddress)
-				.call();
+      await uniswapV2Factory.methods
+        .getPair(tokenAAddress, tokenBAddress)
+        .call();
 
-			const request = {
-				method: 'eth_call',
-				params: [
-					{
-						input: data,
-						to: uniswapV2FactoryAddress,
-					},
-					'latest',
-				],
-			};
-			expect(requestManagerSendSpy).toHaveBeenLastCalledWith(request);
-		});
-	});
+      const request = {
+        method: "eth_call",
+        params: [
+          {
+            input: data,
+            to: uniswapV2FactoryAddress,
+          },
+          "latest",
+        ],
+      };
+      expect(requestManagerSendSpy).toHaveBeenLastCalledWith(request);
+    });
+  });
 
-	describe('Plugin Tests - UniswapV2 - Router2', () => {
-		const requestManagerSendSpy = jest.fn();
+  describe("Plugin Tests - UniswapV2 - Router2", () => {
+    let requestManagerSendSpy: jest.SpyInstance;
 
-		let web3Context: Web3;
-		const uniswapV2RouterAddress = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
+    let web3Context: Web3;
+    const uniswapV2RouterAddress = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
 
-		let uniswapV2Router;
+    let uniswapV2Router;
 
-		// const zeroAddress = '0x0000000000000000000000000000000000000000';
-		// const feeToSetterAddress = '0x18e433c7Bf8A2E1d0197CE5d8f9AFAda1A771360';
+    // const zeroAddress = '0x0000000000000000000000000000000000000000';
+    // const feeToSetterAddress = '0x18e433c7Bf8A2E1d0197CE5d8f9AFAda1A771360';
 
-		beforeAll(() => {
-			web3Context = new Web3();
-			web3Context.setProvider(
-				new Web3.providers.HttpProvider(
-					'https://eth.https://goerli.infura.io/v3/9f9cf6094ac64d658d1282c4fc6d2748-rpc.com'
-				)
-			);
-			web3Context.registerPlugin(new UniswapV2Plugin());
-			web3Context['uniswapV2'].requestManager.send = requestManagerSendSpy;
-			uniswapV2Router = web3Context['uniswapV2'].router2(
-				uniswapV2RouterAddress
-			);
-		});
+    beforeAll(() => {
+      web3Context = new Web3();
+      web3Context.setProvider(
+        new Web3.providers.HttpProvider("https://eth.public-rpc.com")
+      );
+      web3Context.registerPlugin(new UniswapV2Plugin());
 
-		it('should return factory contract address', async () => {
-			const signature = Web3.utils.sha3('factory()')?.slice(0, 10);
-			const data = signature;
+      requestManagerSendSpy = jest.spyOn(
+        web3Context["uniswapV2"].requestManager,
+        "send"
+      );
 
-			const expectedFactoryAddress =
-				'0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
+      uniswapV2Router = web3Context["uniswapV2"].router2(
+        uniswapV2RouterAddress
+      );
+    });
 
-			const factoryAddr = await uniswapV2Router.methods.factory().call();
-			console.log(factoryAddr);
+    it("should return factory contract address", async () => {
+      const signature = Web3.utils.sha3("factory()")?.slice(0, 10);
+      const data = signature;
 
-			const request = {
-				method: 'eth_call',
-				params: [{ input: data, to: uniswapV2RouterAddress }, 'latest'],
-			};
+      const expectedFactoryAddress =
+        "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
 
-			// expect(requestManagerSendSpy).toHaveBeenLastCalledWith(request);
-			expect(factoryAddr).toEqual(expectedFactoryAddress);
-		});
-	});
+      const factoryAddr = await uniswapV2Router.methods.factory().call();
+      console.log(factoryAddr);
+
+      const request = {
+        method: "eth_call",
+        params: [{ input: data, to: uniswapV2RouterAddress }, "latest"],
+      };
+
+      expect(requestManagerSendSpy).toHaveBeenLastCalledWith(request);
+      expect(factoryAddr).toEqual(expectedFactoryAddress);
+    });
+  });
 });
